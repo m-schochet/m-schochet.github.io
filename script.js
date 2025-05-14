3ec2388d363e3321c2ba31397e4e664eee79428e
Meir Schochet <103089439+m-schochet@users.noreply.github.com>
2025-05-14 10:19:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,12 @@
     $(region).show();
     
     // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
+    //$('.main-menu a[href="'+ region +'"]').addClass('active'); 
 
     // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
     // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
-    // $('#content').load(region.slice(1) + '.html')
+     var region = location.hash.toString() || '#first';
+     $('#content').load(region.slice(1) + '.html')
     
   });
   
